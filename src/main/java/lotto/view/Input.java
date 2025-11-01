80744a0c5d906c3d907c4cb1921280a8bd1ebca4
choiseoji <choiseoji123@naver.com>
2025-11-01 16:59:57 +0900

    feat: (Input) 당첨 번호 입력받는 기능 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.LottoException;
 import lotto.validator.LottoValidator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
 
     public int readPurchaseAmount() {
@@ -12,12 +15,29 @@ public class Input {
 
             try {
                 String input = Console.readLine();
-                System.out.println();
 
                 int purchaseAmount = toInt(input);
                 LottoValidator.validateAmount(purchaseAmount);
 
                 return purchaseAmount;
+
+            } catch (LottoException le) {
+                System.out.println(le.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> readWinningNumbers() {
+        while (true) {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+
+            try {
+                String input = Console.readLine();
+
+                List<Integer> numbers = parseNumbers(input);
+                LottoValidator.validateWinningNumbers(numbers);
+
+                return numbers;
 
             } catch (LottoException le) {
                 System.out.println(le.getMessage());
@@ -31,5 +51,12 @@ public class Input {
         } catch (NumberFormatException ne) {
             throw new LottoException("구입 금액은 숫자여야 합니다.");
         }
+    }
+
+    private List<Integer> parseNumbers(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(this::toInt)
+                .toList();
     }
 }
