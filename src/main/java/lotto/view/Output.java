3d013c258f160ef8c85ee425c05f5eb1fc10b115
choiseoji <choiseoji123@naver.com>
2025-11-01 22:46:54 +0900

    feat: (Output) 당첨 내역 및 수익률 출력

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
 import lotto.Lotto;
+import lotto.dto.OneRankResult;
+import lotto.dto.RankResult;
 import lotto.model.Lottos;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,8 +19,26 @@ public class Output {
         }
     }
 
+    public void printWinningStatistics(RankResult rankResult) {
+        System.out.println("\n당첨 통계\n" + "---");
+
+        for (OneRankResult oneRankResult : rankResult.oneRankResults()) {
+            String formattedPrize = String.format("%,d", oneRankResult.prize());
+
+            System.out.println(
+                    oneRankResult.displayName()
+                            + " (" + formattedPrize + "원) - "
+                            + oneRankResult.count() + "개"
+            );
+        }
+
+        DecimalFormat df = new DecimalFormat("#,##0.0");
+        String formattedRate = df.format(rankResult.profitRate());
+        System.out.println("총 수익률은 " + formattedRate + "%입니다.");
+    }
+
     private void printLottoCount(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        System.out.println("\n" + count + "개를 구매했습니다.");
     }
 
     private void printOneLottoNumbers(Lotto lotto) {
@@ -25,6 +46,6 @@ public class Output {
         String result = numbers.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", "));
-        System.out.println("[" + result + "]\n");
+        System.out.println("[" + result + "]");
     }
 }
