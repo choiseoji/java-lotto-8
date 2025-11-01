16d4eb6459d53afccb983a6f8b5f24518bda6ff7
choiseoji <choiseoji123@naver.com>
2025-11-01 17:00:14 +0900

    feat: (LottoValidator) 당첨 번호 유효성 검사하는 기능 추가

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -2,6 +2,10 @@ package lotto.validator;
 
 import lotto.exception.LottoException;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class LottoValidator {
 
     private static final int LOTTO_UNIT = 1000;
@@ -12,6 +16,23 @@ public class LottoValidator {
         }
         if (amount % LOTTO_UNIT != 0) {
             throw new LottoException("구입 금액은 " + LOTTO_UNIT + "원 단위여야 합니다.");
+        }
+    }
+
+    public static void validateWinningNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new LottoException("당첨 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> unique = new HashSet<>(numbers);
+        if (unique.size() != 6) {
+            throw new LottoException("중복된 번호가 있습니다.");
+        }
+
+        for (int num : numbers) {
+            if (num < 1 || num > 45) {
+                throw new LottoException("번호는 1 ~ 45 사이여야 합니다.");
+            }
         }
     }
 }
