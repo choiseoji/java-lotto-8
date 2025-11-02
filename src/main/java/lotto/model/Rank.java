363db06e4dea59bf802d93de023c5b3bb4ac5e55
choiseoji <choiseoji123@naver.com>
2025-11-02 16:46:48 +0900

    refact: (Rank) 당첨된 번호 개수와 보너스 일치 여부를 넘겨주면 일치하는 등수를 스스로 반환하도록 책임 분리

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,27 +1,48 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
 
-    FIFTH(3, 0, 5_000, "3개 일치"),
-    FOURTH(4, 0, 50_000, "4개 일치"),
-    THIRD(5, 0, 1_500_000, "5개 일치"),
-    SECOND(5, 1, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    FIRST(6, 0, 2_000_000_000, "6개 일치");
+    FIFTH(3, false, 5_000, "3개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, false, 2_000_000_000, "6개 일치");
 
     private final int matchCount;
-    private final int bonusMatchCount;
+    private final boolean matchBonus;
     private final int prize;
     private final String displayName;
 
-    public int getMatchCount() { return matchCount; }
-    public int getBonusMatchCount() { return bonusMatchCount; }
-    public int getPrize() { return prize; }
-    public String getDisplayName() { return displayName; }
-
-    Rank(int matchCount, int bonusMatchCount, int prize, String displayName) {
+    Rank(int matchCount, boolean matchBonus, int prize, String displayName) {
         this.matchCount = matchCount;
-        this.bonusMatchCount = bonusMatchCount;
+        this.matchBonus = matchBonus;
         this.prize = prize;
         this.displayName = displayName;
+    }
+
+    public static Rank of(int matchCount, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(r -> r.matchCount == matchCount)
+                .filter(r -> r.matchBonus == bonus || r.matchCount != 5)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean getMatchBonus() {
+        return matchBonus;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 }
