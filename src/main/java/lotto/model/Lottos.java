e491b4006fcf033db28a9ba6e0a0b56adfa6db94
choiseoji <choiseoji123@naver.com>
2025-11-02 16:46:11 +0900

    refact: (Lottos) Lottos 스스로 몇개의 로또가 몇등에 당첨되었는지 계산하도록 수정

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -2,7 +2,9 @@ package lotto.model;
 
 import lotto.Lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
 
@@ -10,6 +12,23 @@ public class Lottos {
 
     public Lottos (List<Lotto> lottos) {
         this.lottos = List.copyOf(lottos);
+    }
+
+    public Map<Rank, Integer> getRankCount(WinningLotto winningLotto) {
+        Map<Rank, Integer> rankCount = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankCount.put(rank, 0);
+        }
+
+        for(Lotto lotto : lottos) {
+            Rank rank = lotto.matchRank(winningLotto);
+            if (rank != null) {
+                int prevCount = rankCount.get(rank);
+                rankCount.put(rank, prevCount + 1);
+            }
+        }
+
+        return rankCount;
     }
 
     public int count() {
